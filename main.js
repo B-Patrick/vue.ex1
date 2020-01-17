5595272dbccb7ce32588d25eafa13cf2abf28b0e
B-Patrick <41405778+B-Patrick@users.noreply.github.com>
2020-01-17 16:16:23 +0200

    computed properties

--- main.js
+++ main.js
@@ -4,21 +4,27 @@ let app = new Vue({
   // vue inctances
 
   data: {
+    brand: "Vue",
     product: "Socks",
-    image: "./assets/green-socks.jpg",
-    inStock: true,
+    // refactoring code
+    //image: "./assets/green-socks.jpg",
+    selectedVariant: 0,
+    // since we have variant qnantity, we refactor instock to computed
+    // inStock: true,
     onSale: true,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [
       {
         variantId: 2234,
         variantColor: "green",
-        variantImage: "./assets/green-socks.jpg"
+        variantImage: "./assets/green-socks.jpg",
+        variantQuantity: 10
       },
       {
         variantId: 2235,
         variantColor: "blue",
-        variantImage: "./assets/blue-socks.jpg"
+        variantImage: "./assets/blue-socks.jpg",
+        variantQuantity: 0
       }
     ],
     sizes: [40, 42, 43, 45, 46],
@@ -32,8 +38,28 @@ let app = new Vue({
     removeFromCart: function() {
       this.cart -= 1;
     },
-    updateProduct: function(variantImage) {
-      this.image = variantImage;
+    // updating product after refactoring
+    updateProduct: function(index) {
+      this.selectedVariant = index;
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    // refactor the deleted image from data into computed property
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    // when hovering over colors will show the availability of the poroduct depending on its quantity
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    sale() {
+      if (this.onSale) {
+        return " / On Sale!";
+      }
+      return " / not on sale";
     }
   }
 });
